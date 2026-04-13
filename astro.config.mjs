d3511d6c1dde1804a17198a51a8842293509755d
Tyac <camilotayac@gmail.com>
2026-04-12 22:23:25 -0500

    chore: configure site base path and fix markdown indentation in astro.config.mjs, and remove swap file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://camilotayac.github.io',
+  base: '/Tyac',
   markdown: {
-      shikiConfig: {
-          // Tema Dracula clásico para los bloques de código
-          theme: 'dracula',
-      },
+    shikiConfig: {
+      // Tema Dracula clásico para los bloques de código
+      theme: 'dracula',
+    },
   },
 
   integrations: [mdx()],
